59bc764257f2baa7372107d6f4de8eb64503acbe
badnewz <mapolupeter2016@gmail.com>
2023-07-09 14:55:43 +0300

    first commit

--- main/views.py
+++ main/views.py
@@ -16,9 +16,11 @@ def home(request):
 
 def register(request):
     if request.method == 'POST':
+        
         username = request.POST['username']
+        email = request.POST['email']
         password = request.POST['password']
-        User.objects.create_user(username=username, password=password)
+        User.objects.create_user(username=username, password=password,email=email)
         messages.success(request, 'Message alert: You have already register!')
         return redirect('login')
     return render(request, 'register.html')
@@ -44,23 +46,25 @@ def logout_view(request):
 
 
 
-@login_required
+
+@login_required(login_url='login')
 def page_vote(request):
-    user = request.user
     candidates = Candidate.objects.all()
     if request.method == 'POST':
         candidate_id = request.POST['candidate']
-        if Vote.objects.filter(user=user).exists():
-            messages.success(request, 'Message alert: You have already voting!')
+        if Vote.objects.filter(user=request.user).exists():
+            messages.success(request, 'Message alert: You have already voted !')
             return redirect('vote')
+    if request.method == 'POST':
+        candidate_id = request.POST['candidate']
         
         Vote.objects.create(user=request.user, candidate_id=candidate_id)
         messages.success(request, 'Message alert: Successfully voting!')
         return redirect('results')
     return render(request, 'vote.html', {'candidates': candidates})
 
-
+@login_required(login_url='login')
 def results(request):
-    votes = Vote.objects.all()
+    votes = Vote.objects.filter(user=request.user)
     return render(request, 'results.html', {'votes': votes})
   
